9f4074ab3b7e919efa5c4fc1e3a4a3794c0e4c5c
Ashley Roarick <ahorseroar@gmail.com>
2019-09-21 20:46:53 -0500

    did some stuff with the pictures

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -7,14 +7,22 @@ $(document).ready(function () {
 });
 
 var selectedPlayer = null;
+var selectedOpponent = null;
 
 function startGame(){
     $("#instructions").text("Choose Your Character");
 }
 
-function characterChoice(that) {
+function characterChoice(that) { 
+    //SELECTS PLAYER
     if (selectedPlayer === null) {
         $(that).insertBefore($("#battleground"));
         selectedPlayer = that;
+        $("#instructions").text("Choose Your Opponent");
+    } else if (selectedOpponent === null) {
+        $(that).insertAfter($("#battleground"));
+        selectedOpponent = that;
+        $("#instructions").text("Fight!"); 
+        
     }
 }
