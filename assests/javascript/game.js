819de61b1e03386035a2f8e7cde25057467ff139
Ashley Roarick <ahorseroar@gmail.com>
2019-09-21 21:01:59 -0500

    put in buttons

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -2,18 +2,27 @@ $(document).ready(function () {
     $(".character").click(function () {
         characterChoice(this);
     });
-
+    $("#attack-button").click(function () {
+        attackButton();
+    });
+    $("#restart-button").click(function(){
+        startGame();
+    });
     startGame();
 });
 
 var selectedPlayer = null;
 var selectedOpponent = null;
 
-function startGame(){
+function startGame() {
+    selectedPlayer = null;
+    selectedOpponent = null;
     $("#instructions").text("Choose Your Character");
+    $("#attack-button").attr("disabled", true);
+    $("#character-choices").append($(".character"));
 }
 
-function characterChoice(that) { 
+function characterChoice(that) {
     //SELECTS PLAYER
     if (selectedPlayer === null) {
         $(that).insertBefore($("#battleground"));
@@ -22,7 +31,11 @@ function characterChoice(that) {
     } else if (selectedOpponent === null) {
         $(that).insertAfter($("#battleground"));
         selectedOpponent = that;
-        $("#instructions").text("Fight!"); 
-        
+        $("#instructions").text("Fight!");
+        $("#attack-button").attr("disabled", false);
     }
+}
+
+function attackButton() {
+    console.log("works");
 }
